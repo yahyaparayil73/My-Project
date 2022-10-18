a0753a287b7a27c14c8a8df4b54a21b478667b60
yahyaparayil73 <yahyaparayil73@gmail.com>
2022-10-18 08:15:53 -0700

    c1

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -52,7 +52,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'Whitenoise.middleware.WhiteNoiseMiddleware',
+    'Whitenoise.middleware.WhiteNoiseMiddleware'
 ]
 
 ROOT_URLCONF = 'ecommerce.urls'
